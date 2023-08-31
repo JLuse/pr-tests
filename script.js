8eb39acf5242491f9ea5d07d34f800846ed4c2f1
Jordan <j.luse7@gmail.com>
2023-08-31 12:24:11 -0700

    Made change to local branch

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let error = document.getElementById('error')
 error.addEventListener('click', function() {
-  console.log("LOGGGGGGG")
+  // console.log("LOGGGGGGG")
+  throw new Error('Parameter is not a number!');
 })
