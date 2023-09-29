ba04778419c2cbc1038f02251ec4f5df5bafc655
Jordan <j.luse7@gmail.com>
2023-09-29 10:30:57 -0700

    This is a test - fixed bug

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let error = document.getElementById('error')
 error.addEventListener('click', function() {
   // console.log("LOGGGGGGG")
-  throw new Error('Parameter is not a number!');
+  // throw new Error('Parameter is not a number!!!');
+  SetFunction()
+  
 })
